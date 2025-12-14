142f333ee45a3511ff1fda9eeee96e4bcd3b7ed5
mostafa gamal <eng.mostafa.goda1@gmail.com>
2025-12-14 21:11:42 +0200

    implement splash screen and icon launcher

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(islami());
+}
+
+class islami extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+
+      },
+    );
+  }
+}
